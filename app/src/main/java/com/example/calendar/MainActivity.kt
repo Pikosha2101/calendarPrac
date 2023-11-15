ebb717bb4e8bcc8083bd7e258785deaa454948e9
pikosha <puka4ybtanke@gmail.com>
2023-11-15 23:53:41 +0300

    .

--- app/src/main/java/com/example/calendar/MainActivity.kt
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -13,11 +13,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var calendarView: CalendarView
     private val friendBirthday = Calendar.getInstance()
     private val NORMAL_CHANNEL = "NORMAL_CHANNEL"
-    private val eventDates: List<Calendar> = listOf(
-        Calendar.getInstance().apply { set(2004, Calendar.JANUARY, 21) },
-        Calendar.getInstance().apply { set(2004, Calendar.MARCH, 4) },
-        Calendar.getInstance().apply { set(2003, Calendar.NOVEMBER, 16) },
+    private val eventDates: ArrayList<BDModel> = arrayListOf (
+        BDModel(Calendar.getInstance().apply { set(2004, Calendar.JANUARY, 21) }, "Олег"),
+        BDModel(Calendar.getInstance().apply { set(2004, Calendar.MARCH, 4) }, "Вася"),
+        BDModel(Calendar.getInstance().apply { set(2003, Calendar.NOVEMBER, 16) }, "Петя"),
+        BDModel(Calendar.getInstance().apply { set(2003, Calendar.JANUARY, 21)}, "Вова")
     )
+
     private lateinit var notificationManager : NotificationManager
 
 
@@ -30,8 +32,13 @@ class MainActivity : AppCompatActivity() {
         calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             friendBirthday.set(year, month, dayOfMonth)
 
-            if (eventDates.any {it.get(Calendar.MONTH) == month && it.get(Calendar.DATE) == dayOfMonth}){
-                createNotification()
+            val matchingEvents = eventDates.filter { it.calendar.get(Calendar.MONTH) == month && it.calendar.get(Calendar.DATE) == dayOfMonth }
+            val list : ArrayList<String> = arrayListOf()
+            if (matchingEvents.isNotEmpty()) {
+                for (event in matchingEvents) {
+                    list.add(event.name)
+                    createNotification(list)
+                }
             } else {
                 deleteNotification()
             }
@@ -40,7 +47,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    private fun createNotification() {
+    private fun createNotification(names : ArrayList<String>) {
         val channel = NotificationChannel(
             NORMAL_CHANNEL,
             resources.getString(R.string.NOT_IMPORTANT_CHANNEL_NAME),
@@ -49,9 +56,9 @@ class MainActivity : AppCompatActivity() {
         channel.enableVibration(false)
 
         notificationManager.createNotificationChannel(channel)
-
+        val nameList = names.joinToString(separator = ",")
         val notification = NotificationCompat.Builder(this, NORMAL_CHANNEL)
-            .setContentTitle("Поздравьте друга!")
+            .setContentTitle("Поздравьтe $nameList!")
             .setContentText("Сегодня день рождения вашего друга.")
             .setSmallIcon(R.drawable.baseline_notifications_24)
             .build()
---
+++ app/src/main/java/com/example/calendar/dataClass.kt
@@ -0,0 +1,6 @@
+package com.example.calendar
+
+//import android.icu.util.Calendar
+import java.util.Calendar
+
+data class BDModel (val calendar : Calendar, val name : String)
